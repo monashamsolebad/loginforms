69cae8a96242a215ce863a202418f3cbed4719ff
Mona Shams <shamsolebad.mona@gmail.com>
2021-07-24 23:06:12 -0700

    css style added

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,66 +1,105 @@
-import React, { useState } from "react";
-function RegistrationForm(props) {
+import React, { useEffect, useState } from "react";
+import styles from "./RegistrationForm.module.css";
+
+function RegistrationForm({ age = 20 }) {
   const [formValues, setFormValues] = useState({
     email: "",
     password: "",
     confirmPassword: "",
   });
+
+  // useEffect(() => {
+  //   // do something when the component mounted, this runs once like the didMount
+  //   return () => {
+  //     // do something when unmount happens
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   // do something here, each time that the formValues changes
+  // }, [formValues]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormValues((prevState) => ({
-      ...prevState,
+    // setFormValues(2);
+    // setFormValues({ ...formValues, [name]: value });
+
+    // const obj1 = { a: 2, b: 3, name: "arman" };
+    // const obj2 = { b: 3 };
+    // const obj3 = { ...obj1, ...obj2 }; // {  a: 2, b: 3, name: "arman", b: 3}
+
+    // const baghali = { ["email"]: "arman" }
+    setFormValues((baghali) => ({
+      ...baghali,
       [name]: value,
     }));
   };
+
   const handleSubmitClick = (e) => {
     e.preventDefault();
-    if (formValues.password === formValues.confirmPassword) {
-      console.log(
-        "email: ",
-        formValues.email,
-        "password: ",
-        formValues.password
-      );
+    // e.stopPropagation();
+    const { password, confirmPassword } = formValues;
+    if (password === confirmPassword) {
+      console.log("email: ", email, "password: ", password);
+      // fetch("/api/register", {
+      //   method: "POST",
+      // body: {
+      //   email,
+      //   psssword,
+      // }
+      // })
     } else {
       console.log("Passwords do not match!");
     }
   };
+
+  const { email, password, confirmPassword } = formValues;
   return (
-    <div>
+    <div className={styles.wrapper}>
       <form>
-        <div>
-          <label>Email Address</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            value={formValues.email}
-            onChange={handleChange}
-          ></input>
+        <div className={styles.innerWrapper}>
+          <div className={styles.formElementWrapper}>
+            <label>Email Address</label>
+            <input
+              className={styles.formElement}
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div className={styles.formElementWrapper}>
+            <label>Password</label>
+            <input
+              className={styles.formElement}
+              disabled
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div className={styles.formElementWrapper}>
+            <label>Confirm Password</label>
+            <input
+              className={styles.formElement}
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <button
+            type="submit"
+            onClick={handleSubmitClick}
+            className={styles.submitFormButton}
+          >
+            Register
+          </button>
         </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formValues.password}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-            value={formValues.confirmPassword}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <button type="submit" onClick={handleSubmitClick}>
-          Register
-        </button>
       </form>
     </div>
   );
